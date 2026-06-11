c3c44a7e39aa245c2925efc8112303f1950e2d69
kyo.lai <kyo.lai82@titansoft.com.sg>
2026-06-11 14:56:52 +0800

    feat(update): ✨ add UpdateCheckRepository.fetchLatest

---
+++ lib/data/repositories/update_check_repository.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/update_info.dart';
+
+class UpdateCheckRepository {
+  UpdateCheckRepository({http.Client? client}) : _client = client ?? http.Client();
+
+  final http.Client _client;
+
+  static const _versionUrl =
+      'https://raw.githubusercontent.com/kyoangel/Daylight/main/version.json';
+
+  Future<UpdateInfo?> fetchLatest() async {
+    final response = await _client.get(Uri.parse(_versionUrl));
+    final json = jsonDecode(response.body) as Map<String, dynamic>;
+    return UpdateInfo.fromJson(json);
+  }
+}
--- test/update_check_repository_test.dart
+++ test/update_check_repository_test.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/testing.dart';
 import 'package:daylight/data/models/update_info.dart';
+import 'package:daylight/data/repositories/update_check_repository.dart';
 
 void main() {
   group('UpdateInfo', () {
@@ -30,6 +34,34 @@ void main() {
       expect(info.isNewerThan(10), isTrue);
       expect(info.isNewerThan(11), isFalse);
       expect(info.isNewerThan(12), isFalse);
+    });
+  });
+
+  group('UpdateCheckRepository', () {
+    test('fetchLatest returns UpdateInfo when the request succeeds', () async {
+      final client = MockClient((request) async {
+        expect(
+          request.url.toString(),
+          'https://raw.githubusercontent.com/kyoangel/Daylight/main/version.json',
+        );
+        return http.Response(
+          jsonEncode({
+            'version': '1.1.0',
+            'buildNumber': 11,
+            'url': 'https://drive.google.com/file/d/abc/view',
+            'notes': '修正已知問題',
+          }),
+          200,
+          headers: const {'content-type': 'application/json; charset=utf-8'},
+        );
+      });
+      final repository = UpdateCheckRepository(client: client);
+
+      final info = await repository.fetchLatest();
+
+      expect(info, isNotNull);
+      expect(info!.version, '1.1.0');
+      expect(info.buildNumber, 11);
     });
   });
 }
